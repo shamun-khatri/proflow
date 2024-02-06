489a0902b0f181478a2f8080db72f6f96ebf7fb0
shamun khatri <shamunkhatri@gmail.com>
2024-02-07 00:01:05 +0530

    Auth Layout Is Created

--- src/app/(main)/agency/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(main)/agency/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,8 +1,11 @@
+import { SignIn } from '@clerk/nextjs'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div>
+      <SignIn />
+    </div>
   )
 }
 
--- src/app/(main)/agency/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ src/app/(main)/agency/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -7,7 +7,6 @@ const page = () => {
   return (
     <>
     <SignUp />
-    <div>lets see</div>
     </>
   )
 }
